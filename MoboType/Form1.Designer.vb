2f5415b3185470227c0f74ae6936eb882aa71e04
Jordan Adjei <jordanadjei360@gmail.com>
2020-08-17 04:55:09 +0000

    Init commit

---
+++ MoboType/Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.bxNum = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.bxAlpha = New System.Windows.Forms.TextBox()
+        Me.lblNC = New System.Windows.Forms.Label()
+        Me.lblAR = New System.Windows.Forms.Label()
+        Me.lblAL = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(177, 302)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear Alpha"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(273, 302)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 23)
+        Me.btnGo.TabIndex = 1
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(369, 302)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 1
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.bxNum)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(297, 276)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Num"
+        '
+        'bxNum
+        '
+        Me.bxNum.Font = New System.Drawing.Font("Franklin Gothic Book", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bxNum.Location = New System.Drawing.Point(6, 19)
+        Me.bxNum.Multiline = True
+        Me.bxNum.Name = "bxNum"
+        Me.bxNum.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.bxNum.Size = New System.Drawing.Size(284, 251)
+        Me.bxNum.TabIndex = 1
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.bxAlpha)
+        Me.GroupBox2.Location = New System.Drawing.Point(314, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(294, 276)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Alpha"
+        '
+        'bxAlpha
+        '
+        Me.bxAlpha.Font = New System.Drawing.Font("Franklin Gothic Book", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bxAlpha.Location = New System.Drawing.Point(6, 19)
+        Me.bxAlpha.MaxLength = 1000
+        Me.bxAlpha.Multiline = True
+        Me.bxAlpha.Name = "bxAlpha"
+        Me.bxAlpha.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.bxAlpha.Size = New System.Drawing.Size(285, 251)
+        Me.bxAlpha.TabIndex = 1
+        '
+        'lblNC
+        '
+        Me.lblNC.AutoSize = True
+        Me.lblNC.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNC.Location = New System.Drawing.Point(18, 291)
+        Me.lblNC.Name = "lblNC"
+        Me.lblNC.Size = New System.Drawing.Size(15, 16)
+        Me.lblNC.TabIndex = 3
+        Me.lblNC.Text = "0"
+        Me.lblNC.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblAR
+        '
+        Me.lblAR.AutoSize = True
+        Me.lblAR.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAR.Location = New System.Drawing.Point(570, 291)
+        Me.lblAR.Name = "lblAR"
+        Me.lblAR.Size = New System.Drawing.Size(36, 16)
+        Me.lblAR.TabIndex = 4
+        Me.lblAR.Text = "1000"
+        Me.lblAR.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        '
+        'lblAL
+        '
+        Me.lblAL.AutoSize = True
+        Me.lblAL.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAL.Location = New System.Drawing.Point(570, 291)
+        Me.lblAL.Name = "lblAL"
+        Me.lblAL.Size = New System.Drawing.Size(15, 16)
+        Me.lblAL.TabIndex = 5
+        Me.lblAL.Text = "0"
+        Me.lblAL.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(620, 337)
+        Me.Controls.Add(Me.lblAL)
+        Me.Controls.Add(Me.lblAR)
+        Me.Controls.Add(Me.lblNC)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.btnClear)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "MoboType - Ready..."
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnGo As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents bxNum As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents bxAlpha As TextBox
+    Friend WithEvents lblNC As Label
+    Friend WithEvents lblAR As Label
+    Friend WithEvents lblAL As Label
+End Class
---
+++ MoboType/Form1.vb
@@ -0,0 +1,134 @@
+﻿'MoboType v1.0
+'
+'
+Public Class Form1
+    Dim inp As String
+    Dim inpLen As Integer
+    Dim y As Integer = 1
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        bxNum.Clear()
+        inp = bxAlpha.Text.ToLower
+        inpLen = inp.Length
+
+        For x = 0 To inpLen - 1
+            'Send to main func
+            catchInp(inp.Substring(x, y))
+            bxNum.AppendText(".")
+        Next
+        bxNum.Text.Remove(bxNum.TextLength - 2, 1)
+    End Sub
+
+    Public Function catchInp(w As String)
+        Dim two() = {"a", "b", "c"}
+        Dim three() = {"d", "e", "f"}
+        Dim four() = {"g", "h", "i"}
+        Dim five() = {"j", "k", "l"}
+        Dim six() = {"m", "n", "o"}
+        Dim seven() = {"p", "q", "r", "s"}
+        Dim eight() = {"t", "u", "v"}
+        Dim nine() = {"w", "x", "y", "z"}
+
+        '3 elements
+        If two.Contains(w) Or three.Contains(w) Or four.Contains(w) Or five.Contains(w) Or six.Contains(w) Or eight.Contains(w) Then
+            For i = 0 To 2
+                If w = two(i) Then
+                    Dim index As Int32 = Array.IndexOf(two, w)
+                    For j = 1 To index + 1
+                        bxNum.AppendText("2")
+                    Next
+
+                ElseIf w = three(i) Then
+                    Dim index As Int32 = Array.IndexOf(three, w)
+                    For j = 1 To index + 1
+                        bxNum.AppendText("3")
+                    Next
+
+                ElseIf w = four(i) Then
+                    Dim index As Int32 = Array.IndexOf(four, w)
+                    For j = 1 To index + 1
+                        bxNum.AppendText("4")
+                    Next
+
+                ElseIf w = five(i) Then
+                    Dim index As Int32 = Array.IndexOf(five, w)
+                    For j = 1 To index + 1
+                        bxNum.AppendText("5")
+                    Next
+
+                ElseIf w = six(i) Then
+                    Dim index As Int32 = Array.IndexOf(six, w)
+                    For j = 1 To index + 1
+                        bxNum.AppendText("6")
+                    Next
+
+                ElseIf w = eight(i) Then
+                    Dim index As Int32 = Array.IndexOf(eight, w)
+                    For j = 1 To index + 1
+                        bxNum.AppendText("8")
+                    Next
+                End If
+            Next
+
+        ElseIf seven.Contains(w) Or nine.Contains(w) Then
+            '4 elements
+            For i = 0 To 3
+                If w = seven(i) Then
+                    Dim index As Int32 = Array.IndexOf(seven, w)
+                    For j = 1 To index + 1
+                        bxNum.AppendText("7")
+                    Next
+
+                ElseIf w = nine(i) Then
+                    Dim index As Int32 = Array.IndexOf(nine, w)
+                    For j = 1 To index + 1
+                        bxNum.AppendText("9")
+                    Next
+                End If
+            Next
+
+        ElseIf w = " " Then
+            bxNum.AppendText("0")
+        End If
+
+        Return 0
+    End Function
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        If btnClear.Text = "Clear Num" Then
+            bxNum.Clear()
+        Else bxAlpha.Clear()
+        End If
+    End Sub
+
+    Private Sub bxNum_Click(sender As Object, e As EventArgs) Handles bxNum.Click
+        btnClear.Text = "Clear Num"
+    End Sub
+
+    Private Sub bxAlpha_Click(sender As Object, e As EventArgs) Handles bxAlpha.Click
+        btnClear.Text = "Clear Alpha"
+    End Sub
+
+    Private Sub bxNum_TextChanged(sender As Object, e As EventArgs) Handles bxNum.TextChanged
+        lblNC.Text = bxNum.TextLength - bxAlpha.TextLength
+    End Sub
+
+    Private Sub bxAlpha_TextChanged(sender As Object, e As EventArgs) Handles bxAlpha.TextChanged
+        lblAR.Text = 1000 - bxAlpha.TextLength
+        lblAL.Text = bxAlpha.TextLength
+    End Sub
+
+    Private Sub lblAR_Click(sender As Object, e As EventArgs) Handles lblAR.Click
+        lblAR.Hide()
+        lblAL.Show()
+    End Sub
+
+    Private Sub lblAL_Click(sender As Object, e As EventArgs) Handles lblAL.Click
+        lblAL.Hide()
+        lblAR.Show()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
